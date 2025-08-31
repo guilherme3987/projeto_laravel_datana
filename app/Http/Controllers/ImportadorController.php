c4daa54275be4a8cbd77bace63b9a9270129f585
guilherme teixeira <guilhermesmt3987@gmail.com>
2025-08-31 15:32:09 -0300

    checando controller

--- app/Http/Controllers/ImportadorController.php
+++ app/Http/Controllers/ImportadorController.php
@@ -8,24 +8,7 @@ use Illuminate\Support\Facades\Session;
 class ImportadorController extends Controller
 {
     public function importar(Request $request) {
-        // Validação e processamento do arquivo de upload
-        $request->validate([
-            'csv_file' => 'required|mimes:csv,txt|max:2048'
-        ]);
-
-        if ($request->file('csv_file')) {
-            $path = $request->file('csv_file')->getRealPath();
-            $data = array_map('str_getcsv', file($path));
-
-            // Aqui você pode processar e salvar os dados no banco de dados
-            // dd($data); // Para verificar os dados
-
-            Session::flash('success', 'Arquivo CSV importado com sucesso!');
-        } else {
-            Session::flash('error', 'Nenhum arquivo enviado.');
-        }
-
-        return redirect()->route('dash');
+        dump("ImportadorController");
     }
 }
 ?>
